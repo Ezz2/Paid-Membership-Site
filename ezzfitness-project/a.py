7e0417a378acc6b22fed4d200f3b0b1c27ba07be
Ezz <94149479+Ezz2@users.noreply.github.com>
2023-11-06 20:28:54 +0200

    Delete ezzfitness-project/a.py

--- ezzfitness-project/a.py
+++
@@ -1,6 +0,0 @@
-percentage = 40
-og_dollar = 800
-
-print(f"Original pricce: {og_dollar}")
-print(f'Discount {percentage}%, Discount Price: {(percentage / 100)* og_dollar}') 
-print(f"Final Price: {og_dollar - (percentage / 100)* og_dollar}")
